ae2c5ed356dd26d054b031ca87f92d6e40c4f889
Jepadox <55612305+Jepadox@users.noreply.github.com>
2021-03-15 22:29:19 -0400

    Visual Studio Commit Test

--- cogs/test.py
+++ cogs/test.py
@@ -15,6 +15,7 @@ class Test(commands.Cog):
         brief="Dev commands for terminal data",
     )
 
+    # dev notes on argument parameters
     # (self, ctx) no arguments needed
     # (self, ctx, arg1, arg2, etc..) list amount of arguments taken, separated by space, quotes make one arg "this is one arg"
     # (self, ctx, *args) take all arguments and put them in library args 
